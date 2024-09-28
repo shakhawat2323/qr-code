019ac62f7d5fe0a08a5a265b0ec50473e910ef6c
shakhawat2323 <shakhawat2323>
2024-09-29 01:12:31 +0600

    update

---
+++ arraytoobject.js
@@ -0,0 +1,17 @@
+// 1 program
+/*১. প্রতিটি সংখ্যাকে দ্বিগুণ করা
+const jone = [23,34,213,53,34,23,34,];
+const myMona = jone.map(jon => jon * 2);
+console.log(myMona)
+*/
+// 2 program
+/*
+const mydos = [23,43,23,4,23,12,43,45];
+const leto = mydos.map(dos => dos / 2);
+console.log(leto)
+*/
+// 3 program
+// ২. প্রতিটি নামকে ক্যাপিটাল লেটারে রূপান্তর করা
+
+const myfrine = ['akash','mion','noniyo','alijabet',];
+const 
---
+++ module31.js
@@ -0,0 +1,169 @@
+// const a = 58;
+// const numbar = [23,45];
+// const person = {
+//     name:'shakhib',
+//     price:12234423,
+
+// }
+// const message = `hi kmon aco dos  ${person.price} dis pric ${a}`
+// // console.log(message)
+
+// const square = x => x *x;
+// // console.log(square(2))
+
+// const sum = (a,b) => a+b;
+// // console.log(sum(23,2))
+
+// const {age, z,...others}= {x:2,y:5,z:3, name:'ovijeet',age:44};
+// // console.log(others,age)
+
+// const [first,secent, ...remaining] = ['rain','sam','joniyo ','deluyes'];
+// // console.log(remaining)
+
+// const data = [
+//     { id:1,  name:'lenvo top',price:23342, },
+//     {id:2,  name:'hp',price:1500, },
+//     {id:3,  name:'Dell',price:5000, },
+//     {id:4,  name:'walton',price:3000, },
+//     {id:5,  name:'asus',price:10250, }
+// ]
+
+// for(let priac of data){
+//     // console.log( priac.hello?.price);
+// }
+
+// //new style function use
+
+// const prodact ={
+//     count:2000,
+//     data:[
+//         { id:1,  name:'lenvo top',price:23342, },
+//         {id:2,  name:'hp',price:1500, },
+//         {id:3,  name:'Dell',price:5000, },
+//         {id:4,  name:'walton',price:3000, },
+//         {id:5,  name:'asus',price:10250, }
+
+//     ]
+
+// }
+
+// // console.log(prodact.data[1]);
+
+// const numbaer = [23,4,3,25,45,3,2,4,5,,445,34,];
+// function doubleit(num){
+//     // console.log('tomar rool',num);
+//     return num * 10;
+// }
+// const result = numbar.map(doubleit);
+// // console.log(result);
+
+
+// // arrow function use kore korci
+// const duboleit = n => n * 5 ;
+// // console.log(duboleit(10))
+
+// const output = numbaer.map(doubleit);
+
+// // console.log(output)
+
+// const output2 = numbaer.map(n => n*2);
+// // console.log(output2)
+
+// const number =[23,43,23,5,4,3,5,3,2,4,354];
+// const dubled = number.map(num => num + 1);
+// // console.log(dubled);
+
+// const friend = ['tometo','joneo','milaio','tometo','joneo','milaio'];
+// // const lengh = frien.map(friend.length);
+// // console.log(lengh);
+
+// const letter =friend.map(friend => friend[0]);
+// // console.log(letter);
+
+
+// // forEach use
+// const number1 =[2,4,34,2,3,4,54,3,4,23,34,54,23,];
+
+// const result1 = number.forEach(n =>{
+
+// })
+// // console.log(result1);
+
+
+// const myNumber = [1,2,3,4,5,6,5,4,43,23,34,45,212];
+// const seleted =myNumber.filter( p => p < 70);
+// // console.log(seleted)
+// const oddNumbar = myNumber.filter( p => p);
+// // console.log(oddNumbar)
+
+// const odd = myNumber.filter(p => p );
+// // console.log(odd);
+// const oddFriend = ['chika ','rumi ','shikha ','mona'];
+// const myfriend = oddFriend.filter(oddFriend => oddFriend.length >4);
+// // console.log(myfriend)
+
+// const raju = ['kmon aco ','ami khub valo aci','din cal kmon cholce otmar'];
+// const myDIn = raju.filter(raju => raju.length < -0);
+// // console.log(myDIn)
+// const mylove = myNumber.find(myNumb => myNumb > -20);
+// // console.log(mylove)
+
+// const numberis = [2,23,34,32,43,34,12,];
+
+// const sum1 = numberis.reduce((a,b) => a +b, 0);
+// // console.log(sum1);
+// let sum12= 0;
+// for(let num= 0; num < numberis.length; num++){
+
+//     sum12 += numberis[num];
+//     // console.log('tomar number',sum12);
+    
+    
+    
+// }
+// // console.log(sum12)
+
+
+// const money = 23;
+// const raich = money + 23
+// // console.log(raich)
+
+
+
+// const aa =23;
+// const b=34;
+// const c = true;
+// const age = [23,34,12,34,];
+// const student = {id:23,class:4};
+// console.log(typeof aa,typeof b, typeof c , typeof age, typeof student);
+
+// let first ;
+// function secend (a,b){
+//     const total = a + b;
+//     return total;
+
+// }
+// const result = secend(2,4);
+// console.log(result)
+
+// function fast(a,b,c,d){
+//     const sum = a + b + c + d;
+//     // console.log(sum)
+//     return sum;
+
+// }
+// const leto = fast(2,4,2);
+// console.log(leto)
+
+// function noNegivastion(a,b){
+//     if(a < 0 || b < 0){
+//         return 
+//     }
+//     return a + b;
+// }
+// const how = noNegivastion(2,-2);
+// console.log(how)
+
+
+// const fifth = {id:2, name:'akhash,',age:233};
+// console.log(fifth.id);
